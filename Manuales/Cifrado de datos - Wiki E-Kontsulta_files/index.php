9a1b4f53179e9e81bb89639a1dc391cf96a01d98
Percy <gsaico@icloud.com>
2014-03-11 00:42:33 -0500

    Manuales para tener una visión mas amplia del tema

---
+++ Manuales/Cifrado de datos - Wiki E-Kontsulta_files/index.php
@@ -0,0 +1,8 @@
+/* generated javascript */
+var skin = 'kontsultabook';
+var stylepath = '/ekontsulta/wiki/skins';
+
+/* MediaWiki:Common.js */
+/* Any JavaScript here will be loaded for all users on every page load. */
+
+/* MediaWiki:Kontsultabook.js (deprecated; migrate to Common.js!) */
